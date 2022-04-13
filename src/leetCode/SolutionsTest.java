284c4764b4f35ad554cb1f90974e82b3c5a0551d
Daniel R <danielsmogorzow1@gmail.com>
2022-04-13 10:44:28 +0200

    RomanInt tests

--- src/leetCode/SolutionsTest.java
+++ src/leetCode/SolutionsTest.java
@@ -10,7 +10,12 @@ class SolutionsTest {
         Solutions solu = new Solutions();
         assertAll("checking some roman values",
         () -> assertEquals(3,solu.romanToInt("III")),
-        () -> assertEquals(4,solu.romanToInt("IV"))
+        () -> assertEquals(4,solu.romanToInt("IV")),
+        () -> assertEquals(1,solu.romanToInt("I")),
+        () -> assertEquals(1555,solu.romanToInt("CMDXCLIXVI")),
+        () -> assertEquals(1555,solu.romanToInt("MDLV")),
+        () -> assertEquals(0,solu.romanToInt("")),
+        () -> assertEquals(12,solu.romanToInt("IXIII"))
         );
     }
 
