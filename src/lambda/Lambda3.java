493d81874f22e5c80864bac101c9b2a331af7cdb
Daniel R <danielsmogorzow1@gmail.com>
2022-04-13 10:35:17 +0200

    Lambda

--- src/lambda/Lambda3.java
+++ src/lambda/Lambda3.java
@@ -1,8 +1,5 @@
 package lambda;
 
-/**
- * Lambda with interface
- */
 public class Lambda3 {
     LambdaInterfaceParameterDouble lambdaField;
 
@@ -14,23 +11,29 @@ public class Lambda3 {
         this.lambdaField = lambdaField;
     }
 
+    /**
+     * Lambda with interfaces, creating interface variables using public interfaces
+     */
     public static void main(String[] args) {
-        // first variable
+        // first interface variable aDouble
         LambdaInterfaceDouble aDouble = () -> 1;
         System.out.println(aDouble.doubleMethod());
 
         System.out.println("---");
 
+        // second variable aDouble1
         LambdaInterfaceDouble aDouble1 = () -> 10;
         System.out.println(aDouble1.doubleMethod());
         System.out.println(aDouble.doubleMethod());
 
         System.out.println("---");
 
+        // different interface used to create parameterDouble interface variable
         LambdaInterfaceParameterDouble parameterDouble = (x, z) -> x * z;
         int result = parameterDouble.intMethodWithParam(4, 2);
         System.out.println(result);
 
+        // reassign expression assigned to parameterDouble
         parameterDouble = (x, z) -> x * 2 * z;
         result = parameterDouble.intMethodWithParam(4, 2);
         System.out.println(result);
@@ -40,13 +43,23 @@ public class Lambda3 {
         // create class and assign expression to interface
         Lambda3 lambda3 = new Lambda3();
         lambda3.lambdaField = (x, z) -> x * z;
-
-        //
         System.out.println(lambda3.lambdaField.intMethodWithParam(4, 3));
 
-        // change interface method by using class setter
+        // change interface method by using class setter method
         lambda3.setLambdaInterfaceParameterDouble((x, z) -> x * z * 2);
         System.out.println(lambda3.lambdaField.intMethodWithParam(4, 3));
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
