4ebe26d1f06e3f104ef299cb6d723e46fa0501e4
Daniel Rogozinski <danielsmogorzow1@gmail.com>
2023-04-09 07:20:38 +0200

    Adding getters and setters

--- src/NewGitEx.java
+++ src/NewGitEx.java
@@ -6,4 +6,20 @@ public class NewGitEx {
         this.name = name;
         this.size = size;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
